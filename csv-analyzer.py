8dd3633b66f7d39c04339416d01ce74badd29717
lkoehl <koehl.lukas@gmx.de>
2019-11-07 09:15:52 +0100

    Counts studiengaenge

--- csv-analyzer.py
+++ csv-analyzer.py
@@ -1,19 +1,35 @@
 import csv
 
+DELIMITER = ";"
+FILE = "list.csv"
 
 
 studiengaenge = []
 
 
-with open('list.csv', mode='r')as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=';')
-    line_count = 0
-    for row in csv_reader:
-        if(line_count == 0):
-            print(row)
-        studiengaenge.append(row["Studiengänge"])
-        line_count += 1;
+def init_studiengaenge():
+    result = {}
+    for studiengang in studiengaenge:
+        splitted_studiengang = studiengang.split(DELIMITER)
+        for single_studiengang in splitted_studiengang:
+            stripped_studiengang = single_studiengang.split(",")[0].strip()
+            if(stripped_studiengang in result):
+                result[stripped_studiengang] = result[stripped_studiengang] + 1
+            else:
+                result[stripped_studiengang] = 1
+
+    print(sorted(result.items(), key = lambda kv:(kv[1], kv[0]), reverse=True))
 
 
-for studiengang in studiengaenge:
-    
+
+def iterate_file():
+    with open(FILE, mode='r')as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=DELIMITER)
+        line_count = 0
+        for row in csv_reader:
+            studiengaenge.append(row["Studiengänge"])
+            line_count += 1;
+    init_studiengaenge()
+
+
+iterate_file()
