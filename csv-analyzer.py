9aa7cb7f21005a2b6c9a435d62c4b3100b86e49c
lkoehl <lkoehl@uos.de>
2019-11-12 16:19:38 +0100

    Adding support for groups

--- csv-analyzer.py
+++ csv-analyzer.py
@@ -9,11 +9,15 @@ students = []
 
 
 class Student:
-    def __init__(self, name, surname, id, login):
+    def __init__(self, group, name, surname, id, login):
+        self.group = group
         self.name = name
         self.surname = surname
         self.id = id
         self.login = login
+
+    def get_group(self):
+        return self.group
 
     def get_name(self):
         return self.name
@@ -33,18 +37,29 @@ def init_studiengaenge():
     print(sorted(result.items(), key=lambda kv: (kv[1], kv[0]), reverse=True))
 
 
-def iterate_file():
-    with open(FILE, mode='r')as csv_file:
+def strip_group(group):
+    elements = group.split(" ")
+    if len(elements) == 5:
+        return -1
+
+    return elements[2]
+
+
+def read_file():
+    with open(FILE, mode='r', encoding='utf-8-sig')as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=DELIMITER)
         for row in csv_reader:
             studiengaenge.append(row["Studiengänge"])
 
+            strip_group(row["Gruppe"])
+
+            group = strip_group(row["Gruppe"])
             name = row["Vorname"]
             surname = row["Nachname"]
             id = row["Nutzernamen"]
             login = row["Anmeldedatum"]
-            student = Student(name, surname, id, login)
+            student = Student(group, name, surname, id, login)
             students.append(student)
 
 
-iterate_file()
+read_file()
